6304b3b2a052d7f29cbd0d4310daff0b9760780e
valiant <junmarkbrilliantes1@gmail.com>
2026-02-26 00:00:48 +0800

    frontend

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
